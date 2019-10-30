ec5dc3e1090ca22e72f8c530d6db66a6fe975564
luisbalru <balderasruizluis@gmail.com>
2019-10-30 14:22:38 +0100

    Ahí va la base de datos #1

--- estadisticas.go
+++ estadisticas.go
@@ -3,20 +3,40 @@ package estadisticas
 import (
           "encoding/json"
           "io/ioutil"
+          "log"
 )
 
 type EstadisticaParo struct {
-    Year Integer
-    NumParados Integer
+    Year Integer `json:"year"`
+    NumParados Integer `json:"parados"`
 }
 
 type Datos struct{
-  Estadisticas []EstadisticaParo
+  Estadisticas []EstadisticaParo `json:"paro"`
 }
+
+var(
+  datos_estadisticos Datos
+)
 
 const default_data_file_name = "./data/ejemplito.json"
 
 func incorporarEstadistica(){
   // Incorpora los datos del paro. El fichero debe estar en la carpeta data
   LeeDatos(default_data_file_name)
+}
+
+func LeeDatos(nombre_archivo string){
+  archivo, e = ioutil.ReadFile(nombre_archivo)
+  if e != nil{
+    log.Fatal("No se puede leer el archivo de datos")
+  }
+
+  if err := json.Unmarshal(archivo, &datos_estadisticos); err != nil{
+    log.Fatal("Error en el JSON ", err)
+  }
+
+
+
+
 }
